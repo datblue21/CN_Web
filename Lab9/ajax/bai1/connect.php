3f8ba0e8fe28b6f32eb9353a7a2701d80cb62a64
datblue21 <92386475+datblue21@users.noreply.github.com>
2022-10-13 14:02:59 +0700

    Lab9

--- Lab9/ajax/bai1/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $local='localhost';
-    $user='root';
-    $pass='';
-    $db='sinhvien';
-    $connect=mysqli_connect($local,$user,$pass,$db);
-    mysqli_set_charset($connect,'utf8');
-?>
